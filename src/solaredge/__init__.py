7258501a12386dc7a5a57483ce9b168cab0c15c6
Elliot Worth <elliotworth@protonmail.com>
2025-08-20 21:50:34 -0500

    fix(__init__): fixed __init__.py from 0.4.0 setup

--- src/solaredge/__init__.py
+++ src/solaredge/__init__.py
@@ -1,5 +1,5 @@
 """Library to interact with SolarEdge's monitoring API."""
 
-from .monitoring import Solaredge
+from .monitoring import MonitoringClient, AsyncMonitoringClient
 
-__all__ = ["Solaredge"]
+__all__ = ["AsyncMonitoringClient", "MonitoringClient"]
